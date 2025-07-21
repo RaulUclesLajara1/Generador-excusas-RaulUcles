e72446dbcb643fa1f1332b3ad092d1a00b7a031f
Raúl Uclés Lajara <rul2@alu.ua.es>
2025-07-21 12:31:26 +0200

    generador de excusas terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-import "bootstrap";
-import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const arr=[
+    ['Mi hermano','Mi tortuga', 'La vecina','Mi suegra','El doctor'],
+    ['meó','abrió','se comió','rompió',],
+    ['el libro','a mi suegra','el ordenador','la mesa','mis apuntes'],
+    ['ayer','mientras jugaba el madrid','hace 3 años','en el 1487']
+  ]
+  let excusa='';
+  arr.forEach(fila => {
+    const random_index= Math.floor(Math.random()*fila.length);
+    excusa += fila[random_index] + ' ';
+    document.querySelector('#excusa').innerHTML = excusa;
+  })
+  
+
 };
